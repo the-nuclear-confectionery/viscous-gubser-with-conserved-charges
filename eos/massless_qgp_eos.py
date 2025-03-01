e83728d66664c46b8b8d0aa58ac6546538afa149
jordissm <jordissm1@gmail.com>
2025-02-28 19:24:57 -0600

    Adapts the MasslessQGPEoS to work with the updated framework

--- eos/massless_qgp_eos.py
+++ eos/massless_qgp_eos.py
@@ -1,56 +1,60 @@
 from typing import Union
-from numpy import pi, ndarray, array, fabs
-from numpy import sum as npsum
+import numpy as np
 from eos.base_eos import BaseEoS
 
 # Degeneracy factor in the QGP
 Nc = 3.0
 Nf = 2.5
 gQCD = 2 * (Nc ** 2 - 1) + 7 * Nc * Nf / 2
-sigma = pi ** 2 / 90
+sigma = np.pi ** 2 / 90
 gq = 4 * Nc * Nf
 ALPHA = gQCD * sigma
 BETA = gq
 BETA1 = BETA / 216
-BETA2 = BETA / (324 * pi ** 2)
+BETA2 = BETA / (324 * np.pi ** 2)
 
 class MasslessQGPEoS(BaseEoS):
     """
     Implements the massless quark-gluon plasma equation of state (EOS1 in the paper)
     with its associated equations.
     """
+
+    # Eq. (12) in the paper
     def pressure(
         self,
-        temperature: Union[float, ndarray],
-        chem_potential: Union[float, ndarray]
-    ) -> Union[float, ndarray]:
-        # Note: Assumes chem_potential is a single number (baryon chemical potential)
-        return_value = ALPHA * temperature ** 4
-        return_value += (BETA1 * chem_potential ** 2 * temperature ** 2)
-        return_value += (BETA2 * chem_potential ** 4)
+        T: Union[float, np.ndarray],
+        mu: Union[float, np.ndarray]
+    ) -> Union[float, np.ndarray]:
+        # Note: mu = (muB, muS, muQ), but we only use muB.
+        return_value = ALPHA * T ** 4
+        return_value += (BETA1 * mu[0] ** 2 * T ** 2)
+        return_value += (BETA2 * mu[0] ** 4)
         return return_value
 
-
+    # Conformal systems have e = 3p
     def energy(
         self,
-        temperature: Union[float, ndarray],
-        chem_potential: Union[float, ndarray]
-    ) -> Union[float, ndarray]:
-        # For a conformal system, e = 3p
-        return 3 * self.pressure(temperature, chem_potential)
-
-    def entropy(
-        self,
-        temperature: Union[float, ndarray],
-        chem_potential: Union[float, ndarray]
-    ) -> Union[float, ndarray]:
-        return_value = (4 * ALPHA * temperature ** 3) + (2 * BETA1 * chem_potential ** 2 * temperature)
-        return return_value
+        T: Union[float, np.ndarray],
+        mu: Union[float, np.ndarray]
+    ) -> Union[float, np.ndarray]:
+        return 3 * self.pressure(T, mu)
 
     def number(
         self,
-        temperature: Union[float, ndarray],
-        chem_potential: Union[float, ndarray]
-    ) -> Union[float, ndarray]:
-        return_value = (2 * BETA1 * chem_potential * temperature ** 2) + (4 * BETA2 * chem_potential ** 3)
+        T: Union[float, np.ndarray],
+        mu: Union[float, np.ndarray]
+    ) -> Union[float, np.ndarray]:
+        # Note: mu = (muB, muS, muQ), but we only use muB.
+        # nS and nQ are always zero.
+        return_value = (2 * BETA1 * mu[0] * T ** 2) + (4 * BETA2 * mu[0] ** 3)
+        return return_value  * np.array([1, 0, 0])
+    
+    # Eq. (14) in the paper
+    def entropy(
+        self,
+        T: Union[float, np.ndarray],
+        mu: Union[float, np.ndarray]
+    ) -> Union[float, np.ndarray]:
+        # Note: mu = (muB, muS, muQ), but we only use muB.
+        return_value = (4 * ALPHA * T ** 3) + (2 * BETA1 * mu[0] ** 2 * T)
         return return_value
