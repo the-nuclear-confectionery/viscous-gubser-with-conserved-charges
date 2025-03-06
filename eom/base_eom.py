2b4f29c361e9692a7acd483b2d2ac93fb0fc23c4
jordissm <jordissm1@gmail.com>
2025-03-06 13:59:44 -0600

    Includes option to use ideal EoM (no pi evolution)

--- eom/base_eom.py
+++ eom/base_eom.py
@@ -15,8 +15,8 @@ class BaseEoM(ABC):
     def dpi_drho(self, ys: np.ndarray, rho: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
         pass
 
-    def eom(self, ys: np.ndarray, rho: Union[float, np.ndarray]) -> np.ndarray:
+    def eom(self, ys: np.ndarray, rho: Union[float, np.ndarray], ideal_evolution: bool = False) -> np.ndarray:
         dT = self.dT_drho(ys, rho)
         dmu = self.dmu_drho(ys, rho)
-        dpi = self.dpi_drho(ys, rho)
+        dpi = self.dpi_drho(ys, rho) if not ideal_evolution else 0
         return np.array([dT, *np.array(dmu).reshape(-1,), dpi])
